f1c43ef7a60d6fb0f56e5a6c65b93d4af24cab52
Gerard <gerard.bourguett@gmail.com>
2022-10-24 13:29:50 -0300

    1.- Hola Mundo

---
+++ 1. Hola Mundo/holamundo.py
@@ -0,0 +1,18 @@
+# Este es un comentario
+
+print ("Hola Mundo")
+print ("      1")
+print ("    2 3")
+print ("  4 5 6")
+print ("7 8 9 10\n")
+
+print (" GGG ", ' BBB ')
+print ("G   G", 'B   B')
+print ("G    ", 'B   B')
+print ("GGGGG", 'BBBBB')
+print ("G   G", 'B   B')
+print ("G   G", 'B   B')
+print (" GGG ", ' BBB ') 
+
+
+"""Este es un comentario"""
